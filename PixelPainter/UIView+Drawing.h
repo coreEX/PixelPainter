0b2fc24a84376e089788d987c9910bba38118ebe
WavingColor <color_free@foxmail.com>
2016-03-19 09:57:29 +0800

    add UIView+Drawing
    drawing methed

---
+++ PixelPainter/UIView+Drawing.h
@@ -0,0 +1,13 @@
+//
+//  UIView+Drawing.h
+//  PixelPainter
+//
+//  Created by mac on 16/3/19.
+//  Copyright © 2016年 DreamDriver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Drawing)
+
+@end
