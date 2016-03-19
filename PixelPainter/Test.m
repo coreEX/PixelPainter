57b8ca67ac368507f5c4e6c7b721a8dedf3823e5
WavingColor <color_free@foxmail.com>
2016-03-19 10:25:18 +0800

    Test

---
+++ PixelPainter/Test.m
@@ -0,0 +1,21 @@
+//
+//  Test.m
+//  PixelPainter
+//
+//  Created by mac on 16/3/19.
+//  Copyright © 2016年 DreamDriver. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
